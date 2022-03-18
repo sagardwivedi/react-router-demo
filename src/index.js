446c3b1291563cb165de595a0b1afef321b935bb
samy-2002 <swagerdavison@gmail.com>
2022-03-19 02:42:19 +0530

    Used Different function

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Expenses from "./routes/expenses";
 import Invoices from "./routes/invoices";
+import Invoice from "./routes/invoice";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -13,7 +14,25 @@ ReactDOM.render(
       <Routes>
         <Route path="/" element={<App />}>
           <Route path="expenses" element={<Expenses />} />
-          <Route path="invoices" element={<Invoices />} />
+          <Route path="invoices" element={<Invoices />}>
+            <Route
+              index
+              element={
+                <main style={{ padding: "1rem" }}>
+                  <p>Select an Invoice</p>
+                </main>
+              }
+            />
+            <Route path=":invoiceId" element={<Invoice />} />
+          </Route>
+          <Route
+            path="*"
+            element={
+              <main style={{ padding: "1rem" }}>
+                <p>There's nothing here!</p>
+              </main>
+            }
+          />
         </Route>
       </Routes>
     </BrowserRouter>
--- src/routes/invoices.jsx
+++ src/routes/invoices.jsx
@@ -1,11 +1,63 @@
+import {
+  NavLink,
+  Outlet,
+  useLocation,
+  useSearchParams,
+} from "react-router-dom";
+import { getInvoices } from "../data";
+
+function QueryNavLink({ to, ...props }) {
+  const location = useLocation();
+  return <NavLink to={to + location.search} {...props} />;
+}
+
 export default function Invoices() {
+  const invoices = getInvoices();
+  const [serachParams, setSearchParams] = useSearchParams();
   return (
-    <main
-      style={{
-        padding: "1rem 0",
-      }}
-    >
-      <h2>Invoices</h2>
-    </main>
+    <div style={{ display: "flex" }}>
+      <nav
+        style={{
+          borderRight: "solid 1px",
+          padding: "1rem",
+        }}
+      >
+      Search : 
+        <input
+          value={serachParams.get("filter") || ""}
+          onChange={(event) => {
+            const filter = event.target.value;
+            if (filter) {
+              setSearchParams({ filter });
+            } else {
+              setSearchParams({});
+            }
+          }}
+        />
+        {invoices
+          .filter((invoice) => {
+            const filter = serachParams.get("filter");
+            if (!filter) return true;
+            const name = invoice.name.toLowerCase();
+            return name.startsWith(filter.toLowerCase());
+          })
+          .map((invoice) => (
+            <QueryNavLink
+              style={({ isActive }) => {
+                return {
+                  display: "block",
+                  margin: "1rem 0",
+                  color: isActive ? "red" : "",
+                };
+              }}
+              to={`/invoices/${invoice.number}`}
+              key={invoice.number}
+            >
+              {invoice.name}
+            </QueryNavLink>
+          ))}
+      </nav>
+      <Outlet />
+    </div>
   );
 }
